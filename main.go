6a050b1917a2712bc7d5b15a05e7e10daab90501
Porrster <porrliam@gmail.com>
2020-11-26 00:28:16 -0800

    CRC webhook verification implemented, still need to verify with twitter api

--- main.go
+++ main.go
@@ -1,79 +1,33 @@
 package main
 
 import (
-	//	"encoding/json"
-	"fmt"
-	"github.com/dghubble/oauth1"
-	"log"
-	"net/http"
-	"os"
-	//	"os/signal"
-	//	"syscall"
+    "fmt"
+    "log"
+    "net/http"
+    "encoding/json"
 )
 
-type Credentials struct {
-	ConsumerKey       string
-	ConsumerSecret    string
-	AccessToken       string
-	AccessTokenSecret string
-}
-
-var USERS_TO_TRACK [1]string = [1]string{"1331444879893942272"}
-
-var TwitterApi string = "https://api.twitter.com/1.1/"
-
 func main() {
-	fmt.Println("James v0.01")
-
-	// Get credentials for twitter create a client
-	creds := Credentials{
-		ConsumerKey:       os.Getenv("CONSUMER_KEY"),
-		ConsumerSecret:    os.Getenv("CONSUMER_SECRET"),
-		AccessToken:       os.Getenv("ACCESS_TOKEN"),
-		AccessTokenSecret: os.Getenv("ACCESS_TOKEN_SECRET"),
-	}
-
-	_, err := getClient(&creds)
-	if err != nil {
-		log.Println("Error getting Twitter Client")
-		log.Println(err)
-	}
-
-	// Should have a similar setup for handling streams
-	//	// Begin handling messages from the stream
-	//	go demux.HandleChan(stream.Messages)
-	//
-	//	// Wait for SIGING and SIGTERM (ctrl-c)
-	//	ch := make(chan os.Signal)
-	//	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	//	log.Println(<-ch)
-	//
-	//	fmt.Println("Stopping stream...")
-	//	stream.Stop()
+    routes()
+    fmt.Println("Starting server...")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// getClient is a helper function that will allow
-// us to stream tweets. It takes in a credentials struct
-// pointer for authentication.
-func getClient(creds *Credentials) (*http.Client, error) {
-	// Credentials for the developer account
-	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
-	// Credentials for the authenticated user
-	token := oauth1.NewToken(creds.AccessToken, creds.AccessTokenSecret)
+func routes() {
+    http.HandleFunc("/webhook/twitter", func(w http.ResponseWriter, r *http.Request) {
+        crcToken, ok := r.URL.Query()["crc_token"]
+        if !ok {
+            fmt.Println("Couldnt get crc_token")
+        }
 
-	httpClient := config.Client(oauth1.NoContext, token)
+        responseToken := generateResponseToken([]byte(crcToken[0]))
+        resp, err := json.Marshal(map[string]string{
+            "response_token": responseToken,
+        })
+        check(err)
 
-	// we can retrieve the user and verify if the credentials
-	// we have used successfullly allow us to log in
-	VerifyCredentials(httpClient)
-
-	return httpClient, nil
-}
-
-func VerifyCredentials(client *http.Client) {
-	resp, err := client.Get(TwitterApi + "account/verify_credentials.json")
-	check(err)
-	fmt.Println(resp)
+        fmt.Fprintln(w, resp)
+    })
 }
 
 // For checking errors more easily
