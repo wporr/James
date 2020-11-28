b3dff717fae1c27af45bef2a16f941d8ff8cb638
Porrster <porrliam@gmail.com>
2020-11-28 12:56:36 -0800

    Able to subscribe to account activity for James's user

--- twitter.go
+++ twitter.go
@@ -23,10 +23,10 @@ type Credentials struct {
 }
 
 type Webhook struct {
-    ID        string
-    URL       string
-    Valid     bool
-    CreatedAt string
+    ID               string `json:"id"`
+    URL              string `json:"url"`
+    Valid            bool   `json:"valid"`
+    CreatedTimestamp string `json:"created_timestamp"`
 }
 
 /* GLOBAL VARIABLES */
@@ -86,29 +86,97 @@ func registerWebhook() {
 		log.Println(err)
 	}
 
-    postURL := TwitterApi
-    postURL.Path = postURL.Path + "/" +
+    webhkEndpt := TwitterApi
+    webhkEndpt.Path = webhkEndpt.Path + "/" +
         url.PathEscape("account_activity") + "/" +
         url.PathEscape("all") + "/" +
         url.PathEscape(ENV_NAME) + "/" +
         url.PathEscape("webhooks.json")
 
-    query := url.Values{}
-    query.Set("url", WEBHOOK_URL)
-    postURL.RawQuery = query.Encode()
-
-    resp, err := client.Post(postURL.String(), "application/json", nil)
+    // First try GET to see if we already have registered webhooks.
+    // Otherwise, POST to register one
+    // We use an app client to get all the webhooks registered for this app,
+    // not just the current user context (represented by `client`)
+    appClient := &http.Client{}
+    req, _ := http.NewRequest("GET", webhkEndpt.String(), nil)
+    req.Header.Set("authorization", "Bearer " + os.Getenv("BEARER_TOKEN"))
+    resp, err := appClient.Do(req)
+    defer resp.Body.Close()
     check(err)
-
-    body, err := ioutil.ReadAll(resp.Body)
-    check(err)
-    fmt.Println(string(body))
-
+    body, _ := ioutil.ReadAll(resp.Body)
     var w = new(Webhook)
-    err = json.Unmarshal([]byte(body), &w)
+
+    // If true, no webhook is registered, make one
+    if string(body) == "[]" {
+        log.Println("No registered webhooks found, registering new one")
+        query := url.Values{}
+        query.Set("url", WEBHOOK_URL)
+        webhkEndpt.RawQuery = query.Encode()
+
+        resp, err := client.Post(webhkEndpt.String(), "application/json", nil)
+        check(err)
+
+        body, _ = ioutil.ReadAll(resp.Body)
+        defer resp.Body.Close()
+        check(json.Unmarshal([]byte(body), &w))
+    } else {
+        var getResp = new([]Webhook)
+        err = json.Unmarshal([]byte(body), &getResp)
+        check(err)
+        *w = (*getResp)[0]
+    }
+
+    check(subscribe(client, ENV_NAME))
+
+//    fmt.Println("deleting webhook")
+//    check(deleteWebhook(w[0].ID, client))
+}
+
+func subscribe(client *http.Client, envName string) error {
+    endpt := TwitterApi
+    endpt.Path = endpt.Path + "/" +
+        url.PathEscape("account_activity") + "/" +
+        url.PathEscape("all") + "/" +
+        url.PathEscape(envName) + "/" +
+        url.PathEscape("subscriptions" + ".json")
+
+    // Check if the subscription exists first
+    resp, err := client.Get(endpt.String())
     check(err)
 
-    fmt.Println(w)
+    if resp.StatusCode == 204 {
+        return nil
+    }
+
+    // Subscription doesnt exist, create it
+    resp, err = client.Post(endpt.String(), "text/plain", nil)
+    check(err)
+
+    if resp.StatusCode != 204 {
+        defer resp.Body.Close()
+        response, _ := ioutil.ReadAll(resp.Body)
+        return errors.New("Could not subscribe environment " +
+            envName + "\nResponse: " + string(response))
+    }
+
+    // This will check to make sure the subscription POST
+    // was processed correctly (GET should return 204)
+    return subscribe(client, envName)
+}
+
+func deleteWebhook(webhookID string, client *http.Client) error {
+    endpt := TwitterApi
+    endpt.Path = endpt.Path + "/" +
+        url.PathEscape("account_activity") + "/" +
+        url.PathEscape("all") + "/" +
+        url.PathEscape(ENV_NAME) + "/" +
+        url.PathEscape("webhooks") + "/" +
+        url.PathEscape(webhookID + ".json")
+
+    req, err := http.NewRequest("DELETE", endpt.String(), nil)
+    check(err)
+    _, err = client.Do(req)
+    return err
 }
 
 // getClient is a helper function that will allow
