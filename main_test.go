8ab689826d49229b751d78a6110f1640a32a73d2
William Porr <wporr@nvidia.com>
2021-01-08 21:58:37 -0800

    Added completion filtering and unsafe text filtering

--- main_test.go
+++ main_test.go
@@ -1,26 +1,78 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	gogpt "github.com/sashabaranov/go-gpt3"
+	"os"
 	"testing"
+	"text/template"
 )
 
-var testResponseChan = make(chan CompletionReponse, 1)
-
-var testRequest = CompletionRequest{
-	Prompt:       "tell me a joke",
-	ResponseChan: testResponseChan,
-	Model:        Davinci,
-	Template:     *StandardTmpl,
-	Temperature:  0.7,
-}
-
 func TestBasicCompletion(t *testing.T) {
+	var testResponseChan = make(chan CompletionReponse, 1)
+	req := CompletionRequest{
+		Prompt:       "tell me a joke",
+		ResponseChan: testResponseChan,
+		Model:        Ada,
+		Template:     *StandardTmpl,
+		Temperature:  0.7,
+	}
+
 	buf := make(chan CompletionRequest, 1)
 	go runCompletions(buf)
 
-	buf <- testRequest
+	buf <- req
 
 	close(buf)
 	fmt.Printf((<-testResponseChan).Response)
+}
+
+func TestSensitivity(t *testing.T) {
+	c := gogpt.NewClient(os.Getenv("OPENAI_API_KEY"))
+	ctx := context.Background()
+
+	text := os.Getenv("UNSAFE_PROMPT")
+	rating, err := checkSensitivity(text, ctx, c)
+
+	if err != nil {
+		t.Errorf("Error when checking sensitivity: %v", err)
+	} else if rating != 2 {
+		t.Errorf("Sensitivity for unsafe prompt was rated: %v", rating)
+	}
+}
+
+func TestSensitivityRetries(t *testing.T) {
+	var testResponseChan = make(chan CompletionReponse, 1)
+	dummyTempl, _ := template.New("dummy").Parse(`{{.Prompt}}`)
+
+	req := CompletionRequest{
+		Prompt:       os.Getenv("UNSAFE_PROMPT"),
+		ResponseChan: testResponseChan,
+		Model:        Ada,
+		Template:     *dummyTempl,
+		Temperature:  0.1,
+	}
+	buf := make(chan CompletionRequest, 1)
+	go runCompletions(buf)
+
+	buf <- req
+
+	close(buf)
+	resp := (<-testResponseChan).Response
+
+	if resp != DEFAULT_RESPONSE {
+		t.Errorf("Unsafe response did not get filtered out, response %v", resp)
+	}
+}
+
+func testFilterDoubleResp(t *testing.T) {
+	txtJames := "James: random response "
+	txtLiam := "\nLiam: remove me"
+
+	filtered := filterResponse(txtJames + txtLiam)
+
+	if filtered != txtJames {
+		t.Errorf("Text did not get filtered properly. Filtered text: %v", filtered)
+	}
 }
--- openai.go
+++ openai.go
@@ -7,6 +7,8 @@ import (
 	gogpt "github.com/sashabaranov/go-gpt3"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 	"text/template"
 )
@@ -16,6 +18,13 @@ import (
 // So to make sure we stay under the limit, we went a bit
 // lower than the tweet char max, from 280/4 to 220/4, i.e. 55
 var MAX_TWEET_TOKENS int = 55
+
+// Number of times we'll retry generating a prompt thats unsafe
+// before giving up
+var MAX_COMPLETION_RETRIES int = 5
+
+// Default response when we reach max retries
+var DEFAULT_RESPONSE string = "*Yaaaawn*... eh, I dont really feel like it"
 
 type CompletionRequest struct {
 	Prompt       string
@@ -89,16 +98,70 @@ func runCompletions(buffer chan CompletionRequest) {
 				Temperature: request.Temperature,
 			}
 
-			resp, err := c.CreateCompletion(ctx, request.Model.String(), req)
-			if err != nil {
-				return
+			try := true
+			respText := ""
+			retries := 0
+
+			for try {
+				resp, err := c.CreateCompletion(ctx, request.Model.String(), req)
+				if err != nil {
+					return
+				}
+
+				respText = resp.Choices[0].Text
+
+				sensitivity, err := checkSensitivity(respText, ctx, c)
+				check(err)
+
+				// Safe is 0, sensitive is 1, unsafe is 2
+				if sensitivity < 2 {
+					try = false
+				} else if retries >= MAX_COMPLETION_RETRIES {
+					respText = DEFAULT_RESPONSE
+					log.Printf("Max retries reached for prompt: %v", req.Prompt)
+					break
+				} else {
+					retries++
+				}
 			}
 
+			filteredText := filterResponse(respText)
+
 			request.ResponseChan <- CompletionReponse{
-				Response: resp.Choices[0].Text,
+				Response: filteredText,
 				Err:      nil,
 			}
 		}
 		close(request.ResponseChan)
 	}
+}
+
+func filterResponse(text string) string {
+	// Regex to match the beginning of text we want to remove
+	// If the ai tries to provide the user's response to it's response,
+	// we'll remove it
+	re := regexp.MustCompile(`\n[a-zA-z0-9]+:`)
+	indexes := re.FindStringIndex(text)
+
+	if indexes != nil {
+		return text[:indexes[0]]
+	}
+	return text
+}
+
+func checkSensitivity(text string, ctx context.Context, c *gogpt.Client) (int, error) {
+	req := gogpt.CompletionRequest{
+		MaxTokens:   1,
+		Prompt:      "<|endoftext|>" + text + "\n--\nLabel:",
+		Temperature: 0.0,
+		TopP:        0,
+	}
+
+	resp, err := c.CreateCompletion(ctx, "content-filter-alpha-c4", req)
+	check(err)
+
+	sensitivity, err := strconv.Atoi(resp.Choices[0].Text)
+	check(err)
+
+	return sensitivity, nil
 }
